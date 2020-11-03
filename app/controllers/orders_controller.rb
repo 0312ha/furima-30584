cf5b35f063193789c97b74db5ff749947a2729f7
0312ha <71496299+0312ha@users.noreply.github.com>
2020-11-03 17:07:52 +0900

    商品購入機能２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order = OrderForm.new
@@ -24,12 +25,18 @@ class OrdersController < ApplicationController
     params.permit(:postalcode_id, :prefecture_id, :city, :housenumber, :phonenumber, :building, :token, :item_id).merge(user_id: current_user.id)
   end
 
-  def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-      amount: @item.price,
-      card: order_params[:token],
-      currency: 'jpy'
-    )
+      def pay_item
+        Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+        Payjp::Charge.create(
+          amount: @item.price,
+          card: order_params[:token],
+          currency: 'jpy'
+        )
+      end
+    def move_to_index
+      @item = Item.find(params[:item_id])
+       if @item.user_id == current_user.id || @item.order.present?
+        redirect_to root_path
+    end
   end
 end
